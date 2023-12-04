95771a70db7c8720f1bcece3e91a09561d324b90
Razenth <santiago.gonzalezagudelo@gmail.com>
2023-12-04 15:55:33 -0500

    feat: :sparkles: All exercises done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,108 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool flag = true;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("----------- MENÚ --------------");
+            Console.WriteLine("1. Mostrar número Fibonacci");
+            Console.WriteLine("2. Verificar si es número Fibonacci");
+            Console.WriteLine("3. Mostrar cierta cantidad de números Fibonacci");
+            Console.WriteLine("4. Salir del Programa");
+            Console.WriteLine("-------------------------------------------");
+            string option = Console.ReadLine();
+            switch (option)
+            {
+                case "1":
+                    Console.Clear();
+                    System.Console.Write("Ingrese número que desee convetir: ");
+                    int num = int.Parse(Console.ReadLine());
+                    if (num < 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Error en número digitado");
+                    }
+                    else
+                    {
+                        int num1 = 0;
+                        int num2 = 1;
+                        for (int i = 0; i < num; i++)
+                        {
+                            int temp = num1;
+                            num1 = num2;
+                            num2 = temp + num1;
+                        }
+                        System.Console.WriteLine("F{0} = {1}", num, num1);
+                    }
+                    break;
+
+                case "2":
+                    Console.Clear();
+                    List<int> list = new List<int>();
+                    System.Console.Write("Ingrese número que desee verificar: ");
+                    int opt = Convert.ToInt32(Console.ReadLine());
+                    if (opt < 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Error en número digitado");
+                    }
+                    else
+                    {
+                        int num1 = 0;
+                        int num2 = 1;
+                        for (int i = 0; i < opt; i++)
+                        {
+                            int temp = num1;
+                            num1 = num2;
+                            num2 = temp + num1;
+                            list.Add(num1);
+                        }
+                        if (list.Contains(opt))
+                        {
+                            System.Console.WriteLine("{0} Si es un número Fibonacci", opt);
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("{0} No es un número Fibonacci", opt);
+                        }
+
+                    }
+                    break;
+                case "3":
+                    Console.Clear();
+                    System.Console.Write("Ingrese cantidad de números Fibonacci a mostrar: ");
+                    int a = int.Parse(Console.ReadLine());
+                    if (a <= 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Error en número digitado");
+                    }
+                    else
+                    {
+                        int f = 0;
+                        int s = 1;
+                        for (int i = 0; i < a; i++)
+                        {
+                            int temp = f;
+                            f = s;
+                            s = temp + f;
+                            System.Console.WriteLine("F{0} = {1}", i + 1, f);
+                        };
+                    }
+                    break;
+
+                case "4":
+                    Console.Clear();
+                    System.Console.WriteLine("Adioos!!");
+                    flag = false;
+                    break;
+            }
+            System.Console.WriteLine("Pulse cualquier tecla");
+            Console.ReadLine();
+        } while (flag);
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # RetoLogica-1
+## Reto Fibonacci 
+En este ejercicio implementé un menú para que la persona al ejecutarlo, pueda seleccionar cualquiera de los 3 puntos que existen en este reto 
+
+```terminal
+----------- MENÚ --------------
+1. Mostrar número Fibonacci
+2. Verificar si es número Fibonacci
+3. Mostrar cierta cantidad de números Fibonacci
+4. Salir del Programa
+-------------------------------------------
+```
+
+- Digite 1 para poder ingresar un número y que imprima el número pero en secuencia Fibonacci
+
+```terminal
+Ingrese número que desee convetir: 11
+F11 = 89
+```
+
+- Digite 2 para ingresar un número y con este indicar si es un número en la secuencia Fibonacci
+
+```terminal
+Ingrese número que desee verificar: 100
+100 No es un número Fibonacci
+```
+
+- Digite 3 para ingresar un número y que muestre un rango hasta el número ingresado
+
+```terminal
+Ingrese cantidad de números Fibonacci a mostrar: 10
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+```
+
+
+
